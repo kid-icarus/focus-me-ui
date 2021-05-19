35cb5067be6e02edaeef54f773698f75a992bf64
Ryan Kois <ryan.kois@gmail.com>
2021-05-18 19:18:55 -0700

    Fix slack plugin ui.

--- src/components/Plugins/Slack/Slack.tsx
+++ src/components/Plugins/Slack/Slack.tsx
@@ -3,11 +3,26 @@ import { Box, Button, FormField, Heading, Paragraph, TextInput } from 'grommet'
 import { PluginProps } from '../index'
 import { FormView, FormViewHide } from 'grommet-icons'
 
+type SlackConfig = Omit<FocusConfig['plugins']['slack'], 'enabled'>
+
 const Slack: React.FC<PluginProps> = ({ config, updatePluginConfig }) => {
-  const { statusEmoji, statusText, token } = config.plugins.slack
+  const [
+    { statusEmoji, statusText, token },
+    setSlackConfig,
+  ] = React.useState<SlackConfig>(config.plugins.slack)
   const [hidden, setHidden] = React.useState(true)
   const toggleHidden = () => {
     setHidden((prev) => !prev)
+  }
+
+  const updateSlackConfig = (partialUpdate: Partial<SlackConfig>) => {
+    const update = {
+      statusEmoji: partialUpdate.statusEmoji ?? statusEmoji,
+      statusText: partialUpdate.statusText ?? statusText,
+      token: partialUpdate.token ?? token,
+    }
+    setSlackConfig(update)
+    updatePluginConfig('slack', update).catch((e) => console.error(e))
   }
 
   return (
@@ -24,6 +39,7 @@ const Slack: React.FC<PluginProps> = ({ config, updatePluginConfig }) => {
               type={hidden ? 'password' : 'text'}
               name="token"
               value={token}
+              onChange={(e) => updateSlackConfig({ token: e.target.value })}
             />
           </FormField>
         </Box>
@@ -34,10 +50,18 @@ const Slack: React.FC<PluginProps> = ({ config, updatePluginConfig }) => {
         />
       </Box>
       <FormField label="Status Emoji">
-        <TextInput name="emoji" value={statusEmoji} />
+        <TextInput
+          name="emoji"
+          value={statusEmoji}
+          onChange={(e) => updateSlackConfig({ statusEmoji: e.target.value })}
+        />
       </FormField>
       <FormField label="Status Text">
-        <TextInput name="" value={statusText} />
+        <TextInput
+          name="statusText"
+          value={statusText}
+          onChange={(e) => updateSlackConfig({ statusText: e.target.value })}
+        />
       </FormField>
     </>
   )
